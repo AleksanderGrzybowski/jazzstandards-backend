88b21974a7b3b7f61b1aa6b9296fb9d77c8f1f0e
Aleksander Grzybowski <kelog@orange.pl>
2017-10-27 20:07:47 +0200

    Empty Spring Boot project

---
+++ src/main/java/pl/kelog/jazzstandards/JazzstandardsApplication.java
@@ -0,0 +1,12 @@
+package pl.kelog.jazzstandards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazzstandardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazzstandardsApplication.class, args);
+	}
+}
